72d7aa957a58304bd777d61e0a52f0b8df5b08d2
VincentNdegwa <ndegwavincent7@gmail.com>
2022-12-09 11:41:20 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (la = 'a'; la <= 'f' la++)
+	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
 
 	putchar('\n');
