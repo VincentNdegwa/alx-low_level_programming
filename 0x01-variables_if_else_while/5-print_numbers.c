0dcd4e89d1e3a1626d69b1157e566e9e3af9ba5a
VincentNdegwa <ndegwavincent7@gmail.com>
2022-12-09 10:26:19 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all strings from 9 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 10; numb++)
+		printsf("\n");
+
+	return (0);
+}
