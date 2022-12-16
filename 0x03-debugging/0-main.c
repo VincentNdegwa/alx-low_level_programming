ad70d6375e4269dcaa4be12809f287a166fb2100
VincentNdegwa <ndegwavincent7@gmail.com>
2022-12-16 05:06:20 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test funtion that prints if negative or positive
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
