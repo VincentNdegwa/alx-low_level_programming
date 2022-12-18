ad8d2b939f1a228c1049b974ed372db0a03d3581
VincentNdegwa <ndegwavincent7@gmail.com>
2022-12-18 09:11:29 -0800

    my alx project

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function thats prints every character of a string
+ * @str: pointer parameter
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
