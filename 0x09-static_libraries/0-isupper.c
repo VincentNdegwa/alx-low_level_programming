32019f96478f7bfe30518dc258d48a1fa191ab5b
VincentNdegwa <ndegwavincent7@gmail.com>
2023-01-09 11:28:52 -0800

    my message

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - chacks if the parameter i an uppercase.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in the case.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
